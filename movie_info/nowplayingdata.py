7c042a5af2297c713c53a0e50d7b839d163bb30b
kmhyunji <klxcxl@gmail.com>
2020-11-23 17:36:53 +0900

    add NowPlayingData

---
+++ movie_info/nowplayingdata.py
@@ -0,0 +1,229 @@
+import json
+
+genre = [
+  {
+    "genre_ids": 28,
+    "name": "액션"
+  },
+  {
+    "genre_ids": 12,
+    "name": "모험"
+  },
+  {
+    "genre_ids": 16,
+    "name": "애니메이션"
+  },
+  {
+    "genre_ids": 35,
+    "name": "코미디"
+  },
+  {
+    "genre_ids": 80,
+    "name": "범죄"
+  },
+  {
+    "genre_ids": 99,
+    "name": "다큐멘터리"
+  },
+  {
+    "genre_ids": 18,
+    "name": "드라마"
+  },
+  {
+    "genre_ids": 10751,
+    "name": "가족"
+  },
+  {
+    "genre_ids": 14,
+    "name": "판타지"
+  },
+  {
+    "genre_ids": 36,
+    "name": "역사"
+  },
+  {
+    "genre_ids": 27,
+    "name": "호러"
+  },
+  {
+    "genre_ids": 10402,
+    "name": "음악"
+  },
+  {
+    "genre_ids": 9648,
+    "name": "미스터리"
+  },
+  {
+    "genre_ids": 10749,
+    "name": "로맨스"
+  },
+  {
+    "genre_ids": 878,
+    "name": "공상 과학"
+  },
+  {
+    "genre_ids": 10770,
+    "name": "TV 영화"
+  },
+  {
+    "genre_ids": 53,
+    "name": "스릴러"
+  },
+  {
+    "genre_ids": 10752,
+    "name": "전쟁"
+  },
+  {
+    "genre_ids": 37,
+    "name": "서부극"
+  }
+]
+
+result = [
+    {
+      "vote_average": 9.08,
+      "title": "삼진그룹 영어토익반",
+      "release_date": "2020-10-21",
+      "original_language": "ko",
+      "original_title": "SAMJIN COMPANY ENGLISH CLASS",
+      "genre_ids": [
+        18
+      ],
+      "backdrop_path": "/n6MaoJEgHkZXc2uVyTQJFncZjho.jpg",
+      "adult": "False",
+      "overview": "입사 8년차 동기인 말단 여직원들이 ‘삼진그룹 영어토익반’에 모인다! 실무 능력 퍼펙트, 현실은 커피 타기 달인인 생산관리3부 오지랖 ‘이자영’(고아성), 추리소설 마니아로 뼈 때리는 멘트의 달인 마케팅부 돌직구 ‘정유나’(이솜), 수학 올림피아드 우승 출신, 실체는 가짜 영수증 메꾸기 달인 회계부 수학왕 ‘심보람’(박혜수)은 대리가 되면 진짜 ‘일’을 할 수 있을 것이라는 희망에 부푼다. 내부고발이라도 하게? 나서지 마. 우리만 다쳐 잔심부름을 하러 간 공장에서 검은 폐수가 유출되는 것을 목격한 ‘자영’은 ‘유나’, ‘보람’과 함께 회사가 무엇을 감추고자 하는지, 결정적 증거를 찾으려 한다. 불가능해 보이는 싸움, 세 친구는 해고의 위험을 무릅쓰고 고군분투를 시작하는데… 아이 캔 두 잇, 유 캔 두 잇, 위 캔 두 잇! 회사와 맞짱 뜨는 용감한 세 친구!",
+      "poster_path": "/5F1UUZjb7BaCQuUbTPBmOb7nlrE.jpg"
+    },
+    {
+      "vote_average": 9.05,
+      "title": "내가 죽던 날",
+      "release_date": "2020-11-12",
+      "original_language": "ko",
+      "original_title": "The day i died : unclosed case",
+      "genre_ids": [
+        18
+      ],
+      "backdrop_path": "/8iYtsUJf295zmEfr2rOHiOrR0kg.jpg",
+      "adult": "False",
+      "overview": "태풍이 몰아치던 밤, 외딴섬 절벽 끝에서 유서 한 장만을 남긴 채 소녀가 사라진다. 오랜 공백 이후 복직을 앞둔 형사 현수는 범죄 사건의 주요 증인이었던 소녀의 실종을 자살로 종결 짓기 위해 그곳으로 향한다. 소녀의 보호를 담당하던 전직 형사, 연락이 두절된 가족, ​그리고 소녀를 마지막으로 목격한 마을 주민 순천댁을 만나 그녀의 행적을 추적해 나가던 현수는 소녀가 홀로 감내했을 고통에 가슴 아파한다. 수사가 진행될수록 자신의 모습과 닮아있는 소녀에게 점점 더 몰두하게 된 현수는 사건 이면에 감춰진 진실 앞에 한걸음 다가서게 되는데…",
+      "poster_path": "/2PpWTaxPR97EDzXcw7sBbnu28lt.jpg"
+    },
+    {
+      "vote_average": 8.57,
+      "title": "도굴",
+      "release_date": "2020-11-04",
+      "original_language": "ko",
+      "original_title": "Collectors",
+      "genre_ids": [
+        80
+      ],
+      "backdrop_path": "/iLIDj4xzIuuFMgKdSOEPRGpCXjB.jpg",
+      "adult": "False",
+      "overview": "흙 맛만 봐도 보물을 찾아내는 타고난 천재 도굴꾼 강동구(이제훈). 자칭 한국의 '인디아나 존스'로 불리는 고분벽화 도굴 전문가 존스 박사(조우진), 전설의 삽질 달인 삽다리(임원희)를 만나 환상(?)의 팀플레이를 자랑하며 위험천만하고도 짜릿한 도굴의 판을 키운다. 한편, 그의 재능을 알아본 고미술계 엘리트 큐레이터 윤실장(신혜선)은 강동구에게 매력적이면서도 위험한 거래를 제안하는데...! 황영사 금동불상, 고구려 고분벽화 그리고 서울 강남 한복판 선릉까지! 팔수록 판이 커지는 도굴의 세계! 급이 다른 삽질이 시작된다!",
+      "poster_path": "/aU7OKmO4qO2pd6TlxC1QQqCkrZQ.jpg"
+    },
+    {
+      "vote_average": 8.83,
+      "title": "런",
+      "release_date": "2020-11-20",
+      "original_language": "en",
+      "original_title": "Run",
+      "genre_ids": [
+        27,
+        9648,
+        53
+      ],
+      "backdrop_path": "/DnjwdbjdR2c5MFhKv5ZSRf8rDU.jpg",
+      "adult": "False",
+      "overview": "태어날 때부터 장애 때문에 휠체어를 타고 외딴 집에서 엄마와 함께 살며 일상을 보내는 ‘클로이’. 딸을 사랑으로 돌보는 엄마 덕분에 힘들지만 매일을 긍정적으로 살아간다. 그러던 어느 날 식탁에 놓인 장바구니에서 하나의 물건을 발견하게 되고 믿었던 모든 일상이 흔들리기 시작하는데…",
+      "poster_path": "/swMFgTEcDUz7MCvWfYIIiZ8PatU.jpg"
+    },
+    {
+      "vote_average": 8.00,
+      "title": "마리 퀴리",
+      "release_date": "2020-11-18",
+      "original_language": "en",
+      "original_title": "Radioactive",
+      "genre_ids": [
+        18,
+        36,
+        10749
+      ],
+      "backdrop_path": "/e7tjpKP36Xd6BzyTMZo4AUe86VM.jpg",
+      "adult": "False",
+      "overview": "뛰어난 연구 실적에도 불구하고 거침없는 성격 때문에 연구실에서 쫓겨난 과학자 ‘마리’. 평소 그녀의 연구를 눈여겨본 ‘피에르’는 공동 연구를 제안하고, 두 사람은 자연스럽게 사랑을 느끼게 된다. 연구를 거듭하던 ‘마리’는 새로운 원소 라듐을 발견하는데 성공하며 ‘피에르’와 함께 노벨상을 수상한다. 그 후 이 발견으로 암을 치료할 수 있다는 기대에 한껏 부푼다. 하지만 기쁨도 잠시, ‘피에르’가 갑작스러운 죽음을 맞고 ‘마리’는 깊은 절망에 빠진다. 그리고 곧 ‘마리’는 이 위대한 발견 이면의 예상치 못한 힘을 알게 되는데… 우리가 미처 몰랐던 ‘마리 퀴리’의 진짜 이야기가 시작된다!",
+      "poster_path": "/sZetjIZ1P4YhaBB93ZSE4Tp7xbH.jpg"
+    },
+    {
+      "vote_average": 8.25,
+      "title": "택스 콜렉터",
+      "release_date": "2020-11-18",
+      "original_language": "en",
+      "original_title": "The Tax Collector",
+      "genre_ids": [
+        28,
+        80,
+        18
+      ],
+      "backdrop_path": "/g0STgveZSRBDihMTDsy8bG1xtl9.jpg",
+      "adult": "True",
+      "overview": "LA 지역 갱들을 관리하며 상납금을 수금하는 최고의 파트너 ‘크리퍼’와 ‘데이비드’ 세상 무서울 것 없는 이들 앞에 조직을 통째로 삼키려는 무법자 ‘코네호’가 나타난다. 지독하고 거칠게 살아남은 두 사람은 조직과 모든 것을 지키기 위해 무자비한 피의 전쟁을 시작하는데…",
+      "poster_path": "/yhhJPePGHGtJ6LBVKCyfJg5EBnA.jpg"
+    },
+    # {
+    #   "vote_average": 7.65,
+    #   "title": "추억의 검정고무신",
+    #   "release_date": "2020-11-19",
+    #   "original_language": "ko",
+    #   "original_title": "The Precious memory of Gogo Brothers",
+    #   "genre_ids": [
+    #     16
+    #   ],
+    #   "backdrop_path": "",
+    #   "adult": "False",
+    #   "overview": "영화티켓이 10원, 소 한 마리가 5만원 하던 시절 장난꾸러기 기영, 기철 형제 가족의 평범하고도 특별한 일상이 펼쳐진다.",
+    #   "poster_path": "/GUv9mRFRSEwaC2hQEMeOu9mHrj.jpg"
+    # },
+]
+
+
+# dumpdata 하기 전에 손 봐야 함!
+# 1. models.py에 NowPlaying model 설정하기
+# 2. 아래 코드 수정하기
+# 3. dumpdata json 파일 만들고 loaddata!
+
+# dump = []
+# for j in range(len(genres)):
+#   dump.append(
+#     {
+#       "model": "movie_info.genre",
+#       "pk": genres[j]['genre_ids'],
+#       "fields": {
+#         'name': genres[j]['name']
+#       }
+#     }
+#   )
+
+# for i in range(1, len(result)):
+#   dump.append(
+#     {
+#       "model": "movie_info.movie",
+#       "pk": i,
+#       # "fields": result[i]
+#       "fields": {
+#         'vote_average': result[i]['vote_average'],
+#         'title': result[i]['title'],
+#         'release_date': result[i]['release_date'],
+#         'original_language': result[i]['original_language'],
+#         'original_title': result[i]['original_title'],
+#         'genre_ids': result[i]['genre_ids'],
+#         'backdrop_path': result[i]['backdrop_path'],
+#         'adult': result[i]['adult'],
+#         'overview': result[i]['overview'],
+#         'poster_path': "https://image.tmdb.org/t/p/w500" + result[i]['poster_path'],
+#       }
+#     }
+#   )
+
+# with open('nowplaying.json', 'w', encoding='UTF-8') as file:
+#   file.write(json.dumps(dump, ensure_ascii=False))
